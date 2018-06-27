857015535c362a52f3bbbe689e0d404c8eef47cb
Yojesuschrist <40617835+Yojesuschrist@users.noreply.github.com>
2018-06-27 09:54:31 -0700

    Add files via upload

---
+++ js_starter-master/js/index.js
@@ -0,0 +1,32 @@
+
+// controls for changing the background
+document.getElementById('background-btn').addEventListener('click', function() {
+    document.getElementById('background').style.backgroundImage = "url('/background.jpg')";
+});
+
+
+
+
+
+// button controls
+
+// controls for the left button
+document.getElementById('left').addEventListener('click', function() {
+
+        var position = document.getElementById('ball').style.left;
+    
+        var newPosition = parseInt(position) - 10;
+    
+        document.getElementById('ball').style.left = newPosition + 'px';
+});
+
+// controls for the right button
+document.getElementById('right').addEventListener('click', function() {
+
+    var position = document.getElementById('ball').style.left;
+
+    var newPosition = parseInt(position) + 10;
+
+    document.getElementById('ball').style.left = newPosition + 'px';
+});
+
